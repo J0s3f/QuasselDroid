9dd257eb3d611ed5e62c4b4b6130c0228dd0d7de
Andrew Gillard <lorddeath@gmail.com>
2012-04-07 22:01:09 +0100

    Fixed an issue whereby pressing the "back" button on the buffer-list screen would return you to the login screen, and where you would be taken to the login screen when re-opening the app even when already connected (this especially impacted my sharing-via-intents feature).

--- src/com/iskrembilen/quasseldroid/gui/LoginActivity.java
+++ src/com/iskrembilen/quasseldroid/gui/LoginActivity.java
@@ -151,7 +151,10 @@ public class LoginActivity extends Activity implements Observer, DialogInterface
 
 			@Override
 			protected void onReceiveResult(int resultCode, Bundle resultData) {
-				if (resultCode==CoreConnService.CONNECTION_CONNECTING) {
+				if (resultCode==CoreConnService.CONNECTION_CONNECTED) {
+					// Already connected (probably the app being launched again while already connected)
+					LoginActivity.this.startActivity(new Intent(LoginActivity.this, BufferActivity.class));
+				} else if (resultCode==CoreConnService.CONNECTION_CONNECTING) {
 					removeDialog(R.id.DIALOG_CONNECTING);
 					Intent bufferIntent = new Intent(LoginActivity.this, BufferActivity.class);
 					if (sharedString != null && sharedString.length() > 0) {
